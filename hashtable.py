b9e664a771f2c17caa87412bd5f9b4e3ae322436
Gustav Jenner <jennergustav@gmail.com>
2022-03-03 09:02:39 +0100

    # final code + commentarer

--- hashtable.py
+++ hashtable.py
@@ -1,61 +1,57 @@
+#DictHash Klass
 class DictHash:
-    def __init__(self):
+    def __init__(self): #init metod för DictHash
         self._dict = {}
 
-    def store(self, key, data):
+    def store(self, key, data): #store metod
         self._dict[key] = data
 
-    def search(self, key):
+    def search(self, key):#search metod
         return self._dict[key]
 
-    def __getitem__(self, key):
+    def __getitem__(self, key):#getitem retunerar search
         return self.search(key)
 
-    def __contains__(self, item):
+    def __contains__(self, item): #contains
         return item in self._dict.values()
 
-
+#HashNode Klass
 class HashNode:
-    def __init__(self, key = " ", data = None, next = None):
+    def __init__(self, key=" ", data=None): #Init metod med 3 attribute nyckel, data och en pekare för krocklista
         self.key = key
         self.data = data
-        self.next = next
+        self.next = None
 
-
+#Hashtable klass
 class Hashtable:
 
+    def __init__(self, size):# Init metod för Hashtable
+        self._table = [None] * 2 * size #dubbla storleken på listan för att minska risken för krock
 
-    def __init__(self, size):
-        self._table = [None] * 2 * size
-
-
-    def store(self, key, data):
-        if self._table[self.hashfunction(key)] is None:
+    def store(self, key, data):#Store metod
+        if self._table[self.hashfunction(key)] is None: #lägger in om ingen krock
             self._table[self.hashfunction(key)] = HashNode(key, data)
         else:
-            help_store(self._table[self.hashfunction(key)], HashNode(key, data))
+            help_store(self._table[self.hashfunction(key)], HashNode(key, data))#kallar hjälpfunktion om det blir krock
 
-
-
-    def search(self, key):
-        if self._table[self.hashfunction(key)] is None:
+    def search(self, key):#Search metod
+        if self._table[self.hashfunction(key)] is None: #Om nyckeln inte finns
             raise KeyError
 
-        elif self._table[self.hashfunction(key)].key == key:
+        elif self._table[self.hashfunction(key)].key == key:#Om nyckel finns på första position
             return self._table[self.hashfunction(key)].data
         else:
-            return help_search(self._table[self.hashfunction(key)], key)
+            return help_search(self._table[self.hashfunction(key)], key) #kallar hjälp funktion,hitta i krocklista
 
-
-    def hashfunction(self, key):
+    def hashfunction(self, key):#Hash metod
         hash_value = 0
         for i, ele in enumerate(key):
-            hash_value += 8 ** i * ord(ele)
-        hash_value *= hash_value
-        return hash_value % len(self._table)
+            hash_value += 8 ** i * ord(ele) #tar 8^i*ord(bokstav)
+        hash_value *= hash_value #kvadrerar värdet
+        return hash_value % len(self._table)#retunerar mod av längden av listan så indexet blir rätt
 
 
-def help_store(current_node, insert_node):
+def help_store(current_node, insert_node): #rekursivt lägger värdet på rättplats
     if current_node.key == insert_node.key and insert_node.data is not None:
         current_node.data = insert_node.data
 
@@ -65,10 +61,10 @@ def help_store(current_node, insert_node):
         help_store(current_node.next, insert_node)
 
 
-def help_search(current_node, key):
-    if current_node.next is None:
+def help_search(current_node, key):#rekursivt söker igenom krocklistan
+    if current_node.next is None:#Om hela krocklistan söks igenom finns inte nyckeln
         raise KeyError
-    elif current_node.next.key == key:
+    elif current_node.next.key == key: #nyckeln hittades
         return current_node.next.data
     else:
         return help_search(current_node.next, key)
